217c4e4ebf05f37eb5d8c3de163193ac3ed47050
nishita246 <agrawalnishita36@gmail.com>
2024-10-26 19:35:49 +0530

    Task-2 Create home view and return HTTPResponse

--- app_food/app_food/urls.py
+++ app_food/app_food/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def hello_world(request):
+    return HttpResponse("Welcome to git")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',hello_world)
 ]
